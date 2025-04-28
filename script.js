84b848ab2e69aaa2b8cc1e1ecf8facbd71d97296
ahmed-dev2030 <aazmy5979@gmail.com>
2025-04-28 18:57:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// بسيطة الآن، يمكنك لاحقاً تضيف أنيميشن أو فورم إرسال
+console.log('Landing page ready to roll!');
+
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
